d74be200f3a67da0a0d431591e92fe3409c92a7b
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2020-05-29 17:06:28 +0800

    Update V2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh

--- V2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
+++ V2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
@@ -1493,7 +1493,7 @@ start_menu()
                 exit 1
             fi
             rm -rf "$0"
-            wget -O "$0" "https://github.com/kirin10000/V2Ray-WebSocket-TLS-Web-setup-script/raw/master/V2ray-WebSocket(ws)+TLS(1.3)+Web-setup-beta.sh"
+            wget -O "$0" "https://github.com/kirin10000/V2Ray-WebSocket-TLS-Web-setup-script/raw/master/V2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh"
             chmod +x "$0"
             "$0" --update
             ;;
