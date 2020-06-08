21adb4ea5fa131379c46b19630a48d6d5ac4c761
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2020-06-09 00:11:16 +0800

    bug-fixs

--- V2ray-WebSocket(ws)+TLS(1.3)+Web-setup-beta.sh
+++ V2ray-WebSocket(ws)+TLS(1.3)+Web-setup-beta.sh
@@ -1162,11 +1162,11 @@ install_update_v2ray_ws_tls()
     ./configure --prefix=/etc/nginx --with-openssl=../$openssl_version --with-openssl-opt="enable-tls1_3 enable-tls1_2 enable-tls1 enable-ssl enable-ssl2 enable-ssl3 enable-ec_nistp_64_gcc_128 shared threads zlib-dynamic sctp" --with-mail=dynamic --with-mail_ssl_module --with-stream=dynamic --with-stream_ssl_module --with-stream_realip_module --with-stream_geoip_module=dynamic --with-stream_ssl_preread_module --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_geoip_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_auth_request_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-pcre --with-libatomic --with-compat --with-cpp_test_module --with-google_perftools_module --with-file-aio --with-threads --with-poll_module --with-select_module --with-cc-opt=-O3
     make
     if [ $update == 1 ]; then
-        mkdir domain_backup
+        mkdir ../domain_backup
         for i in ${!domain_list[@]}
         do
             if [ ${pretend_list[i]} == 1 ]; then
-                mv /etc/nginx/html/${domain_list[i]} domain_backup
+                mv /etc/nginx/html/${domain_list[i]} ../domain_backup
             fi
         done
     fi
