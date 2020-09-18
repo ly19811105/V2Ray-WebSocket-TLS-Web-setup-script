575b3a5814f49f499e5b34ef8c5a3a2c68368a20
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2020-09-18 11:01:03 +0800

    紧急修复一个bug

--- V2Ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
+++ V2Ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
@@ -343,7 +343,7 @@ install_update_v2ray_ws_tls()
     fi
     apt -y -f install
     check_important_dependence_installed ca-certificates ca-certificates
-    check_important_dependence_installed lsb-release lsb_release
+    check_important_dependence_installed lsb-release redhat-lsb-core
     #系统版本
     systemVersion=`lsb_release -r --short`
     systemctl stop nginx
